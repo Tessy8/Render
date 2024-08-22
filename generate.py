0de0d9d81fcc2c0af46bc350cac9a93dc63b7fbb
Tessy8 <adamstestimony@gmail.com>
2024-08-22 20:39:25 +0100

    Created array of towers

--- generate.py
+++ generate.py
@@ -5,9 +5,22 @@ width = 1
 height = 1
 x = 0
 y = 0
-z = 0.5
+z = 0.4
 
 pyrosim.Start_SDF("boxes.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[x+length,y,z+height], size=[length,width,height])
+for k in range(5):
+	for j in range(5):
+		for i in range(10):
+			length = 0.9*length
+			width = 0.9*width
+			height = 0.9*height
+			pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
+			z = z + height
+		z = 0.4
+		length = 1
+		width = 1
+		height = 1
+		x = x + length + 0.5
+	x = 0
+	y = y + width + 0.5
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -10,7 +10,7 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 p.loadSDF("boxes.sdf")
 
-for i in range(1000):
+for i in range(10000):
     p.stepSimulation()
     time.sleep(1/60)
     print(f"Iteration: {i}")
