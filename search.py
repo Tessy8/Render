e8f271cd68e350b92a82e19d3eeb4fa1976e4c91
Tessy8 <adamstestimony@gmail.com>
2024-09-21 23:30:41 +0100

    Random Search

---
+++ search.py
@@ -0,0 +1,5 @@
+import os
+
+for i in (0,1,2,3,4):
+	os.system("python generate.py")
+	os.system("python simulate.py")
--- simulation.py
+++ simulation.py
@@ -31,7 +31,7 @@ class SIMULATION:
 			self.robot.Sense(i)
 			self.robot.Think()
 			self.robot.Act(i)
-			time.sleep(1/50)
+			time.sleep(1/500)
 		
 
 	def __del__(self):
